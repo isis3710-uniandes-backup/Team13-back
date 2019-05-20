c9f35f0767e205f8168296376aba56cb5a20c4bb
Santiago Múnera <sf.munera10@uniandes.edu.co>
2019-05-19 21:12:56 -0500

    pwa changes

--- server.js
+++ server.js
@@ -1,19 +1,37 @@
 // Import packages
 const express = require('express');
+const path = require('path');
+var cors = require("cors");
 
 // App
 const app = express();
+const port = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(require('./routes/server.routes'))
+app.use(require('./routes/server.routes'));
 
+//Static file declaration
+app.use(express.static(path.join(__dirname, 'Team13-front/build')));
 
-const port = '3001';
+//production mode
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'Team13-front/build')));
+  //
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'Team13-front/build/index.html'));
+  })
+}
+//build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/Team13-front/public/index.html'));
+})
 
-// Starting server
-app.listen(port);
-console.log("Listening on port: "+port);
+//start server
+app.listen(port, (req, res) => {
+  console.log( `server listening on port: ${port}`);
+})
 
 //Export for testing
 module.exports = app;
